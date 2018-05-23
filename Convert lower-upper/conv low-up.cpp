4cbd6cc350e09642d3cf8af87879d85eff3a8744
Yash Garg <ben10.yashgarg@gmail.com>
2018-05-23 10:12:35 +0530

    Add Convert low-up
    Code for converting lower to upper and vice versa !

---
+++ Convert lower-upper/conv low-up.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+using namespace std;
+char name[30];
+ifstream fin;
+int i;
+
+void convert () { 							//Loop for converting Upper to lower and vice versa
+	for (i=0;i<=strlen(name);i++) {
+		if (islower(name[i])) {
+			name[i] = (toupper(name[i]));
+		}
+		else if (isupper(name[i])) {
+			name[i] = (tolower(name[i]));
+		}
+	}
+}
+
+int main () {
+	fin.open("main.txt" , ios :: in);
+	fin.getline(name , 30);
+	convert();
+	cout<<"Converted text : ";
+	puts(name);
+	fin.close();
+	ofstream fout("convert.txt" , ios :: out);
+	fout<<name;
+	fout.close();
+	return 0;
+}
