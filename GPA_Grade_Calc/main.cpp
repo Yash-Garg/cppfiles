5e0e4b09a8ee8ae6e5fd8d14bdd14245bf1b9578
Yash Garg <ben10.yashgarg@gmail.com>
2018-06-25 19:17:58 +0530

    Update main.cpp

--- GPA_Grade_Calc/main.cpp
+++ GPA_Grade_Calc/main.cpp
@@ -10,8 +10,17 @@ class std_details {
 	string name;
 	string ID;
 	char sec;
-	int s_class;
+	int std_class;
 	public:
 		std_details();
-		
+		void get_details(int stdno);
+};
+
+class std_marks : public std_details{ 	//Deriving the base class publically
+	float perc[nsub], gpa[nsub], cgpa;
+	public:
+		std_marks();
+		float calc_perc(int i); 		//calculates the percentage in each subject
+		float calc_gpa(int j); 			//calculates the GPA in each subject
+		float calc_cgpa(); 				//calculates the CGPA in each subject		
 };
