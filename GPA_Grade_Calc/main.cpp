319bc08292126b9198697ec2f9314d250519c4ff
Yash Garg <ben10.yashgarg@gmail.com>
2018-06-25 19:10:35 +0530

    Start project !

---
+++ GPA_Grade_Calc/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<windows.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+class std_details {
+	string name;
+	string ID;
+	char sec;
+	int s_class;
+	public:
+		std_details();
+		
+};
