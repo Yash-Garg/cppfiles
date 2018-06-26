efc56287b61549ec9b0ac8d206d4143bd2939bb7
Yash Garg <ben10.yashgarg@gmail.com>
2018-06-26 20:16:08 +0530

    Update main.cpp

--- GPA_Grade_Calc/main.cpp
+++ GPA_Grade_Calc/main.cpp
@@ -4,15 +4,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
+#include<ctype.h>
 using namespace std;
 int i;
 class std_details {
-	float perc[2], gpa[2], tmarks;
+	float perc[2], gpa[2], tmarks, cgpa, sbmarks[2];
 	char name[30], sec;
-	int cgpa, stdclass, stdno, sbmarks[2];
+	int stdclass, stdno;
 	public:
 		void get_details();				//Get the details of student
-		void get_marks();				//Gets the marks for subjects
+		void get_marks_gpa_perc();			//Gets the marks for subjects
 		float calc_cgpa(); 				//Calculates the CGPA in each subject
 		float calc_grade(); 				//Calculates the Grade in each subject
 		void show_out();				//Shows the output to user
@@ -34,9 +35,9 @@ void std_details :: get_details () {
 	cin>>sec;
 }
 
-void std_details :: get_marks () {
+void std_details :: get_marks_gpa_perc () {
     tmarks = 100 ;						//Assigns the total marks 100
-	cgpa = 0;
+	cgpa == 0;
 	for(int i=0 ; i<=2 ; i++) {
 		cout<<"\n"<<"Enter the marks for subject "<<i+1<<" : ";
 	    cin>>sbmarks[i];
@@ -45,11 +46,15 @@ void std_details :: get_marks () {
 	}
 }
 
+/*void std_details :: calc_cgpa () {
+	
+}*/
+
 void std_details :: show_out () {
 	system("cls");
 	cout<<"\n"<<"Student's Name : ";
 	puts(name);
-	cout<<"\n"<<"Roll Number : "<<stdno<<endl;
+	couts<<"\n"<<"Roll Number : "<<stdno<<endl;
 	cout<<"\n"<<"Class : "<<stdclass<<endl;
 	cout<<"\n"<<"Section : "<<sec<<endl;	
 		for(int i=0 ; i<=2 ; i++) {
@@ -61,7 +66,7 @@ void std_details :: show_out () {
 
 int main () {
 	init.get_details();
-	init.get_marks();
+	init.get_marks_gpa_perc();
 	init.show_out();
 	return 0;
 }
