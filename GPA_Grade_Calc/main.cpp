dc1f738022924e3af9c50787dcd900b0d6792cfc
Yash Garg <ben10.yashgarg@gmail.com>
2018-06-26 20:02:59 +0530

    Update main.cpp

--- GPA_Grade_Calc/main.cpp
+++ GPA_Grade_Calc/main.cpp
@@ -13,11 +13,9 @@ class std_details {
 	public:
 		void get_details();				//Get the details of student
 		void get_marks();				//Gets the marks for subjects
-		float calc_perc(); 				//Calculates the percentage in each subject
-		float calc_gpa(); 				//Calculates the GPA in each subject
 		float calc_cgpa(); 				//Calculates the CGPA in each subject
-		float calc_grade(); 			//Calculates the Grade in each subject
-		void show_out();
+		float calc_grade(); 				//Calculates the Grade in each subject
+		void show_out();				//Shows the output to user
 }init;
 
 void std_details :: get_details () {
@@ -37,13 +35,13 @@ void std_details :: get_details () {
 }
 
 void std_details :: get_marks () {
-    tmarks = 100 ;					//Assigns the total marks 100
+    tmarks = 100 ;						//Assigns the total marks 100
 	cgpa = 0;
 	for(int i=0 ; i<=2 ; i++) {
 		cout<<"\n"<<"Enter the marks for subject "<<i+1<<" : ";
 	    cin>>sbmarks[i];
-		perc[i] = sbmarks[i]*100/tmarks;
-		gpa[i] = perc[i] * 5 / 100;
+		perc[i] = sbmarks[i]*100/tmarks;		//Calculates the percentage in each subject
+		gpa[i] = perc[i] * 5 / 100;			//Calculates the GPA in each subject
 	}
 }
 
@@ -56,7 +54,7 @@ void std_details :: show_out () {
 	cout<<"\n"<<"Section : "<<sec<<endl;	
 		for(int i=0 ; i<=2 ; i++) {
 			cout<<"\n"<<"Marks of subject "<<i+1<<" : "<<sbmarks[i]<<endl;
-			cout<<"\n"<<"Percentage in subject "<<i+1<<" : "<<perc[i]<<endl;
+			cout<<"\n"<<"Percentage in subject "<<i+1<<" : "<<perc[i]<<"%"<<endl;
 			cout<<"\n"<<"GPA in subject "<<i+1<<" : "<<gpa[i]<<endl;
 		}
 }
