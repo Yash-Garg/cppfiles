70799f83e7fba677b86d734d42ef55328f13a39a
Yash Garg <ben10.yashgarg@gmail.com>
2018-06-26 21:55:57 +0530

    Add the restart function

--- GPA_Grade_Calc/main.cpp
+++ GPA_Grade_Calc/main.cpp
@@ -7,13 +7,14 @@
 #include<ctype.h>
 using namespace std;
 int i;
+char response;
 class std_details {
 	float perc[3], gpa[3], tmarks, cgpa, sbmarks[3];
 	char name[30], sec, grade;
 	int stdclass, stdno;
 	public:
-	public:
 		void start();
+		void restart();
 		void get_details();							//Get the details of student
 		void get_marks_gpa_perc();						//Gets the marks for subjects
 		void show_out();							//Shows the output to user
@@ -41,8 +42,9 @@ void std_details :: get_marks_gpa_perc () {
     tmarks = 100 ;									//Assigns the total marks 100
     cgpa = 0;
 	for(int i=0 ; i<3 ; i++) {
+		Sleep(500);
 		cout<<"\n"<<"Enter the marks for subject "<<i+1<<" : ";
-	   	cin>>sbmarks[i];
+		cin>>sbmarks[i];
 		perc[i] = ( sbmarks[i] / tmarks ) * 100.0 ;				//Calculates the percentage in each subject	
 		gpa[i] = ( perc[i] * 5.0 ) / 100.0;					//Calculates the GPA in each subject
 		cgpa += gpa[i] / 3 ;							//Calculates the CGPA of student
@@ -69,25 +71,58 @@ float std_details :: calc_grade () {
 
 void std_details :: show_out () {
 	system("cls");
+	Sleep(500);
 	cout<<"\n"<<"Student's Name : ";
 	puts(name);
+	Sleep(500);
 	cout<<"\n"<<"Roll Number : "<<stdno<<endl;
+	Sleep(500);
 	cout<<"\n"<<"Class : "<<stdclass<<endl;
+	Sleep(500);
 	cout<<"\n"<<"Section : "<<sec<<endl;	
 		for(int i=0 ; i<3 ; i++) {
+			Sleep(500);
 			cout<<"\n"<<"Marks of subject "<<i+1<<" : "<<sbmarks[i]<<endl;
+			Sleep(500);
 			cout<<"\n"<<"Percentage in subject "<<i+1<<" : "<<perc[i]<<"%"<<endl;
+			Sleep(500);
 			cout<<"\n"<<"GPA in subject "<<i+1<<" : "<<gpa[i]<<endl;
 		}
+	Sleep(500);
 	cout<<"\n"<<"CGPA of "<<name<<" is : "<<cgpa<<endl;
+	Sleep(500);
 	cout<<"\n"<<"Grade of "<<name<<" is : "<<grade;
+}
+
+void std_details :: restart () {
+	Sleep(500);
+	cout<<endl;
+	cout<<"\n"<<"Do you want to re-run the program (Y/N) ?? : ";
+	cin>>response;
+	if (response == 'Y' || response == 'y') {
+		system("cls");
+		start();
+		cout<<endl<<"THANKS FOR USING THE PROGRAM !!";
+		cout<<endl;
+	}
+	else if (response == 'N' || response == 'n') {
+		cout<<endl<<"THANKS FOR USING THE PROGRAM !!";
+		cout<<endl;
+		exit(0);
+	}
+	else {
+		cout<<"\n"<<"AW SNAP !! WRONG INPUT !!";
+		cout<<endl;
+		exit(0);
+	}
 }
 
 void std_details :: start () {
 	get_details();
 	get_marks_gpa_perc();
 	calc_grade();
-	show_out();
+	show_out();	
+	restart();
 	Sleep(500);
 }
 
