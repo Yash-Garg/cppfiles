c4b0ef37d1567b5509a54afac929c3388d8bbc20
Yash Garg <ben10.yashgarg@gmail.com>
2018-07-02 15:17:21 +0530

    Add some files !

---
+++ this_these_filestream/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<windows.h>
+using namespace std;
+
+int main () {
+	ifstream fin;
+	fin.open("new.txt", ios :: in);
+    char x[30];
+    int y, z;
+    while ( !fin.eof()) {
+        fin>>x;
+        if(strcmpi(x , "this")==0) {
+            y++;
+        }
+        if(strcmpi(x , "these")==0) {
+            z++;
+        }
+    }
+    cout<<"\nCount of 'This' is "<<y;
+    cout<<"\nCount of 'These' is "<<z;
+    fin.close();
+}
