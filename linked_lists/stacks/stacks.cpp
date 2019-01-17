c75cd54cdab70662c3e098c2fddd23cd2c3c476c
Yash Garg <ben10.yashgarg@gmail.com>
2019-01-17 19:15:26 +0530

    Update stacks.cpp

--- linked_lists/stacks/stacks.cpp
+++ linked_lists/stacks/stacks.cpp
@@ -9,18 +9,17 @@ struct NODE {
 
 };
 
-class stack
-{
-private:
-	NODE *TOP;
-public:
-	stack() { TOP = NULL; };
-	void push();
-	void pop();
-	void display();
-	~stack();
-
+class stack {
+	private:
+		NODE *TOP;
+	public:
+		stack() { TOP = NULL; };
+		void push();
+		void pop();
+		void display();
+		~stack();
 };
+
 void stack::push() {
 	NODE *TEMP;
 	TEMP = new NODE;
@@ -28,12 +27,10 @@ void stack::push() {
 	cout << "Enter data"; cin >> TEMP->data;
 	TEMP->NEXT = TOP;
 	TOP = TEMP;
-	
 }
 
 void stack::pop() {
-	if (TOP!= NULL)
-	{
+	if (TOP!= NULL) {
 		NODE *TEMP = TOP;
 		TOP = TOP->NEXT;
 		cout << TEMP->data << " Element Deleted\n";
@@ -49,42 +46,34 @@ void stack::display() {
 		cout << TEMP->data << " ";
 		TEMP = TEMP->NEXT;
 	}
-	
 }
 
-stack::~stack()
-{
+stack::~stack() {
 	while (TOP != NULL) {
 		NODE *TEMP = TOP;
 		TOP=TOP->NEXT;
 		delete TEMP;}
-
 }
 
-int main()
-{
+int main() {
 	system("cls");
 	stack st;
 	int ch;
 
-	do
-	{
+	do {
 		cout << "\n\nMain Menu :";
 		cout << "\n1.PUSH ELEMENT";
 		cout << "\n2.POP ELEMENT";
 		cout << "\n3.DISPLAY STACK";
 		cout << "\nENTER YOUT CHOICE: ";
 		cin >> ch;
-		switch (ch) 
-		{
-		case 1: st.push();break;
-		case 2: st.pop();break;
-		case 3: st.display();break;
-		default: exit(0);break;
-	}
-
+		switch (ch) {
+			case 1: st.push();break;
+			case 2: st.pop();break;
+			case 3: st.display();break;
+			default: exit(0);break;
+		}
 	} while (ch<4);
 	system("pause");
     return 0;
 }
-
